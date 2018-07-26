932f280fd124d66ca982d8b45d95a2d7849c7b6f
Vmussato <vinicius.mussato@gmail.com>
2018-07-25 23:41:40 -0300

    Fim do primeiro módulo - estrutura de componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'alurapic';
+  description = 'Leão';
+  url = 'https://upload.wikimedia.org/wikipedia/commons/4/4d/BarbaryLionB1898bw.jpg';
 }
