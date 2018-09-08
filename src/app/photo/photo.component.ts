13c44503837e93906ebed9505f6108f12f068129
Vmussato <vinicius.mussato@gmail.com>
2018-09-07 22:53:21 -0300

    Added inbound properties

--- src/app/photo/photo.component.ts
+++ src/app/photo/photo.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'app-photo',
     templateUrl: 'photo.component.html'
 })
 export class PhotoComponent {
-    description = 'Leão';
-    url = 'https://upload.wikimedia.org/wikipedia/commons/4/4d/BarbaryLionB1898bw.jpg';
+    @Input() description = '';
+    @Input() url = '';
 }
