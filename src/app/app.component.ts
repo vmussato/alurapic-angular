cf24b1efe2f21b6556f9b6c8047ef27a4a03f55f
Vmussato <vinicius.mussato@gmail.com>
2018-09-09 23:56:37 -0300

    Added ngonInit and imported HttpClientModule on photos.module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PhotoService } from './photos/photo/photo.service';
 
 
@@ -7,16 +7,14 @@ import { PhotoService } from './photos/photo/photo.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   photos: any[] = [];
 
-  constructor(photoService: PhotoService) {
+  constructor(private photoService: PhotoService) { }
 
-    photoService.
-      listFromUser('flavio')
-      .subscribe(photos => {
-        console.log(photos[0].description);
-        this.photos = photos;
-      });
+  ngOnInit(): void {
+    this.photoService
+      .listFromUser('flavio')
+      .subscribe(photos => this.photos = photos);
   }
 }
