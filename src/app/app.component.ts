eeec1bd6c4bbcdbe4a11bba77660c40a62bdb616
Vmussato <vinicius.mussato@gmail.com>
2018-09-09 23:24:42 -0300

    Added photo service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { PhotoService } from './photos/photo/photo.service';
+
 
 @Component({
   selector: 'app-root',
@@ -9,11 +10,10 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent {
   photos: Object[] = [];
 
-  constructor(http: HttpClient) {
+  constructor(photoService: PhotoService) {
 
-    http
-      .get<Object[]>('http://localhost:3000/flavio/photos')
-      .subscribe(
-        photos => this.photos = photos);
+    photoService.
+      listFromUser('flavio')
+      .subscribe(photos => this.photos = photos);
   }
 }
