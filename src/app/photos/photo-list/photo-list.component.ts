d13dc16f20f567f16c7b0f7c0e66d9d3dabac596
Vmussato <vinicius.mussato@gmail.com>
2018-09-15 13:53:07 -0300

    Added a debounce to avoid excessive filters

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
+
 import { Photo } from '../photo/photo';
 
 
@@ -9,11 +11,12 @@ import { Photo } from '../photo/photo';
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy {
 
   photos: Photo[] = [];
   // tslint:disable-next-line:no-inferrable-types
   filter: string = '';
+  debounce: Subject<string> = new Subject<string>();
 
   constructor(
     private activatedRoute: ActivatedRoute
@@ -21,6 +24,12 @@ export class PhotoListComponent implements OnInit {
 
   ngOnInit(): void {
     this.photos = this.activatedRoute.snapshot.data.photos;
+    this.debounce
+      .pipe(debounceTime(300))
+      .subscribe(filter => this.filter = filter);
   }
 
+  ngOnDestroy(): void {
+    this.debounce.unsubscribe();
+  }
 }
