455a5f7f9f87ed5c8ecf9aabe436857a05f903c5
Vmussato <vinicius.mussato@gmail.com>
2018-09-15 19:55:49 -0300

    Created sub modules for components

---
+++ src/app/photos/photo-form/photo-form.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { PhotoFormComponent } from './photo-form.component';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+    declarations: [
+        PhotoFormComponent
+    ],
+    imports: [CommonModule]
+})
+export class PhotoFormModule {
+
+}
