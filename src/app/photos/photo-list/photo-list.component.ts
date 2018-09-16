6b951d5df2fe92c52738a75386c06ffd961044ba
Vmussato <vinicius.mussato@gmail.com>
2018-09-16 01:15:04 -0300

    Added communication between modules using eventEmitter

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,7 +1,5 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Subject } from 'rxjs';
-import { debounceTime } from 'rxjs/operators';
 
 import { Photo } from '../photo/photo';
 import { PhotoService } from '../photo/photo.service';
@@ -12,12 +10,11 @@ import { PhotoService } from '../photo/photo.service';
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit, OnDestroy {
+export class PhotoListComponent implements OnInit {
 
   photos: Photo[] = [];
   // tslint:disable-next-line:no-inferrable-types
   filter: string = '';
-  debounce: Subject<string> = new Subject<string>();
   hasMore = true;
   currentPage = 1;
   userName = '';
@@ -30,18 +27,12 @@ export class PhotoListComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.userName = this.activatedRoute.snapshot.params.userName;
     this.photos = this.activatedRoute.snapshot.data.photos;
-    this.debounce
-      .pipe(debounceTime(300))
-      .subscribe(filter => this.filter = filter);
-  }
-
-  ngOnDestroy(): void {
-    this.debounce.unsubscribe();
   }
 
   load() {
     this.photoService.listFromUserPaginated(this.userName, ++this.currentPage)
       .subscribe(photos => {
+        this.filter = '';
         this.photos = this.photos.concat(photos);
         if (!photos.length) {
           this.hasMore = false;
--- src/app/photos/photo-list/photo-list.module.ts
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -6,6 +6,7 @@ import { FilterByDescription } from './filter-by-description.pipe';
 import { CommonModule } from '@angular/common';
 import { PhotoModule } from '../photo/photo.module';
 import { CardModule } from '../../shared/components/card/card.module';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
     declarations: [
@@ -13,6 +14,7 @@ import { CardModule } from '../../shared/components/card/card.module';
         PhotosComponent,
         LoadButtonComponent,
         FilterByDescription,
+        SearchComponent
     ],
     imports: [
         CommonModule,
